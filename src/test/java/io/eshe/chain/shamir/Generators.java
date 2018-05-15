06b9b1eb2b651a6a2cdf86691b7968433d79e15a
Steven Enamakel <enamakel@cryptocontrol.io>
2018-05-15 19:57:20 +0530

    Formatted code

--- src/test/java/io/eshe/chain/shamir/Generators.java
+++ src/test/java/io/eshe/chain/shamir/Generators.java
@@ -21,24 +21,23 @@ import org.quicktheories.impl.Constraint;
 import java.util.Arrays;
 
 public interface Generators {
+    static Gen<Byte> bytes(int minValue, int maxValue) {
+        return prng -> ((byte) prng.next(Constraint.between(minValue, maxValue)));
+    }
 
-  static Gen<Byte> bytes(int minValue, int maxValue) {
-    return prng -> ((byte) prng.next(Constraint.between(minValue, maxValue)));
-  }
+    static Gen<Byte> bytes() {
+        return bytes(0, 255);
+    }
 
-  static Gen<Byte> bytes() {
-    return bytes(0, 255);
-  }
-
-  static Gen<byte[]> byteArrays(int minSize, int maxSize) {
-    final Gen<byte[]> gen =
-        prng -> {
-          final byte[] bytes = new byte[(int) prng.next(Constraint.between(minSize, maxSize))];
-          for (int i = 0; i < bytes.length; i++) {
-            bytes[i] = (byte) prng.next(Constraint.between(0, 255));
-          }
-          return bytes;
-        };
-    return gen.describedAs(Arrays::toString);
-  }
+    static Gen<byte[]> byteArrays(int minSize, int maxSize) {
+        final Gen<byte[]> gen =
+                prng -> {
+                    final byte[] bytes = new byte[(int) prng.next(Constraint.between(minSize, maxSize))];
+                    for (int i = 0; i < bytes.length; i++) {
+                        bytes[i] = (byte) prng.next(Constraint.between(0, 255));
+                    }
+                    return bytes;
+                };
+        return gen.describedAs(Arrays::toString);
+    }
 }
